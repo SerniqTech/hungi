720d120c8dcd73ff3d137645ac81472b8cf0c522
PHANI <phani@phanindras-MacBook-Airlocal.local>
2026-02-05 18:16:19 +0530

    fixed the buildings rls

--- supabase/migrations/20260203163426_08_policies_users_and_buildings.sql
+++ supabase/migrations/20260203163426_08_policies_users_and_buildings.sql
@@ -13,17 +13,8 @@ create policy "Admin full access users" on users for all using (get_my_role () =
 -- =====================================================
 -- BUILDINGS
 -- =====================================================
-create policy "Customer sees own building" on buildings for
-select
-    using (
-        id = (
-            select
-                building_id
-            from
-                users
-            where
-                id = auth.uid ()
-        )
-    );
 
-create policy "Admin full access buildings" on buildings for all using (get_my_role () = 'admin');
+create policy "Public read buildings"
+on buildings
+for select
+using (true)
